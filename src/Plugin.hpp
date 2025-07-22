2faf81247a3ee96b20fdaa8158f62ed25f1a28df
Jan Galek <ghome.cz@gmail.com>
2025-07-22 18:43:35 +0200

    try debug mode

--- src/Plugin.hpp
+++ src/Plugin.hpp
@@ -201,13 +201,14 @@ namespace GOTHIC_NAMESPACE
 				//auto r = npc->GetFromInv(npc->instanz, rawMeatIndex);
 				auto rr = player->GetIDByInstance("ITMI_RAWMEAT");
 				auto rrr = player->GetIDByInstance("ITFO_MEAT");
+				auto rrrItem = player->GetFromInv(rrr, 0);
 				//player->GetFromInv()
 				//new oCItem();
 				//DebugLog("Máš " + std::to_string(rawMeat->objectName) + " syrového masa.");
 
 				//int rawInstance = rawMeat->GetInstance();
-					DebugLog("Instance " + std::to_string(rawMeat->amount) + ", " + std::to_string(rr) + " syrového masa.");
-					DebugLog("Instance " + std::to_string(rrr) + ", " + std::to_string(rrr) + " pecene masa.");
+					DebugLog("Instance " + std::to_string(rrr) + ", " + std::to_string(rr) + " syrového masa.");
+					DebugLog("Instance " + std::to_string(rrr) + ", " + std::to_string(rrrItem->GetCount(0)) + " pecene masa.");
 				if (rawMeat) {
 					int rawCount = rawMeat->GetValue();
 					DebugLog("Máš " + std::to_string(rawCount) + " syrového masa.");
